3971f943f9adac48d87316006966902efbfaa8a8
Patricia <waltraute@hotmail.com>
2022-04-05 10:02:46 +0200

    actualitzat

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-let Jugadora = require("./class_Jugadora").default;
+let Jugadora = require("./class_Jugadora.js");
 
-arrayjugadores = [
-     new Jugadora("Jugadora", 20, 198, 98, "b"),
-     new Jugadora("Sieglinde", 20, 198, 98, "b"),
-     new Jugadora("Waltraute", 20, 198, 98, "b"),
-     new Jugadora("Woglinde", 20, 198, 98, "b"),
-     new Jugadora("Ortrude", 20, 198, 98, "b"),
-     new Jugadora("Brunhilde", 20, 198, 98, "b"),
-     new Jugadora("Fricka", 20, 198, 98, "b"),
-     new Jugadora("Erda", 20, 198, 98, "b")
-]
+arrayJugadores = [
+  new Jugadora("Sigrun", 20, 198, 98, "b"),
+  new Jugadora("Sieglinde", 20, 198, 98, "b"),
+  new Jugadora("Waltraute", 20, 198, 98, "b"),
+  new Jugadora("Woglinde", 20, 198, 98, "b"),
+  new Jugadora("Ortrude", 20, 198, 98, "b"),
+  new Jugadora("Brunhilde", 20, 198, 98, "b"),
+  new Jugadora("Fricka", 20, 198, 98, "b"),
+  new Jugadora("Erda", 20, 198, 98, "b"),
+  new Jugadora("Perica", 20, 198, 98, "jgjggjb"),
+];
 
-
-try {
-     let Jugadora = new Jugadora("Jugadora", 20, 198, 98, "xghzb");
+/*try {
+  let Jugadora =  new Jugadora("Waltraute", 20, 198, 98, "bjhfksdgfjh");
 } catch (error) {
-     console.log("ERROR: " + error.message)
-}
-
+  console.log("ERROR: " + error.message);
+}*/
 
 // console.log(arrayJugadores);
-console.log("Mes de 2 m")
+console.log("Mes de 2 m");
 for (let index = 0; index < arrayJugadores.length; index++) {
-     if (arrayjugadores[index].h > 200) {
-          console.log(arrayjugadores[index].nom, arrayjugadores[index].h);
-     }
+  if (arrayJugadores[index].h > 200) {
+    console.log(arrayJugadores[index].nom, arrayJugadores[index].h);
+  }
 }
 
-console.log("Menys de 21 anys")
+console.log("Menys de 21 anys");
 
-for (let index = 0; index < arrayjugadores.length; index++) {
-     if (arrayjugadores[index].edat < 21) {
-          console.log(arrayjugadores[index].nom, arrayjugadores[index].edat);
-     }
+for (let index = 0; index < arrayJugadores.length; index++) {
+  if (arrayJugadores[index].edat < 21) {
+    console.log(arrayJugadores[index].nom, arrayJugadores[index].edat);
+  }
 }
